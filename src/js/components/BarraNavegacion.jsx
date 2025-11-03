22ebfc3589309eba43ec02707c3f6b36a848d067
santiago-pp <santiagopereaap@gmail.com>
2025-11-03 20:32:00 +0000

    Proyecto TERMINADO

--- src/js/components/BarraNavegacion.jsx
+++ src/js/components/BarraNavegacion.jsx
@@ -2,30 +2,22 @@ import React from "react";
 
 export default function BarraNavegacion() {
     return (
-        <nav className="navbar bg-light">
-            <div className="container-fluid ">
-                <a className="navbar-brand" href="#">
-                    Start Bootstrap
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <div className="container-fluid">
+                <div className="justify-content-between">
+                <a className="navbar-brand text-white" href="#">Start Bootstrap</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-            </div>
-            <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Service</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
+                </div>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ">
+                        <a className="nav-link active text-white" aria-current="page" href="#">Home</a>
+                        <a className="nav-link text-white" href="#">About</a>
+                        <a className="nav-link text-white" href="#">Services</a>
+                        <a className="nav-link text-white" href="#">Contact</a>
+                    </div>
+                </div>
             </div>
         </nav>
     )
